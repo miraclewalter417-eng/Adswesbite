d4ba558c8f664ae5e0f020618c689c70dcd62483
miracle walter <miraclewalter417@gmail.com>
2026-02-11 18:18:58 +0100

    sdfg

--- script.js
+++ script.js
@@ -13,33 +13,67 @@ let ads = JSON.parse(localStorage.getItem("myAds")) || [
     price: 500,
     category: "Services",
     desc: "Modern UI/UX",
-  }
+  },
 ];
-
 const adsGrid = document.getElementById("adsGrid");
 const adForm = document.getElementById("adForm");
 const modal = document.getElementById("adModal");
+const removeBtn = document.getElementById("removeBtn");
+
+// Modal Controls
+
+document.getElementById("openModal").onclick = () =>
+  (modal.style.display = "block");
+document.querySelector(".close").onclick = () => (modal.style.display = "none");
+window.onclick = (e) => {
+  if (e.target == modal) modal.style.display = "none";
+};
+
+// Simple Search
+
+function searchAds() {
+  const term = document.getElementById("searchInput").value.toLowerCase();
+  const filtered = ads.filter((ad) => ad.title.toLowerCase().includes(term));
+  renderAds(filtered);
+}
+
+// Category Filter
+
+function filterCategory(cat) {
+  if (cat === "All") return renderAds();
+  const filtered = ads.filter((ad) => ad.category === cat);
+  renderAds(filtered);
+}
+
+async function searchAds() {
+  const term = document.getElementById("searchInput").value;
+  const cat = document.querySelector(".active-category")?.innerText || "All";
+
+  // This calls the server with specific "query parameters"
+  try {
+    const response = await fetchProducts(
+      `${"https:fakestoreapi.com/products"}`,
+    );
+    const data = await response.json();
+    // renderAds(filteredAds);
+  } catch (error) {
+    console.error("error fetching products:", error);
+  }
+}
 
 // Render Ads to Screen
+
 function renderAds(data = ads) {
-  adsGrid.innerHTML = data
-    .map(
-      (ad) => `
-        <div class="ad-card">
-            <div style="height:150px; background:#e2e8f0; display:flex; align-items:center; justify-content:center; color:#94a3b8">No Image</div>
-            <div class="ad-info">
-                <small>${ad.category}</small>
-                <h3>${ad.title}</h3>
-                <p class="ad-price">$${ad.price}</p>
-                <p>${ad.desc.substring(0, 50)}...</p>
-            </div>
-        </div>
-    `,
-    )
-    .join("");
+  console.log(data);
+  const rawHtml = data.sort((a, b) => b.id - a.id).map((ad) => cardItem(ad));
+  // console.log(rawHtml);
+  adsGrid.innerHTML = "";
+  adsGrid.append(...rawHtml);
+  // adsGrid.appendChild(rawHtml[0]);
 }
 
 // Handle Form Submission
+
 adForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -52,77 +86,86 @@ adForm.addEventListener("submit", (e) => {
   };
 
   ads.push(newAd);
-  localStorage.setItem("myAds", JSON.stringify(ads)); 
-  
+  localStorage.setItem("myAds", JSON.stringify(ads));
+
   // Save to browser memory
+
   renderAds();
   adForm.reset();
   modal.style.display = "none";
 });
 
-// Modal Controls
-document.getElementById("openModal").onclick = () =>
-  (modal.style.display = "block");
-document.querySelector(".close").onclick = () => (modal.style.display = "none");
-window.onclick = (e) => {
-  if (e.target == modal) modal.style.display = "none";
-};
-
-// Simple Search
-function searchAds() {
-  const term = document.getElementById("searchInput").value.toLowerCase();
-  const filtered = ads.filter((ad) => ad.title.toLowerCase().includes(term));
-  renderAds(filtered);
-}
-
-// Category Filter
-function filterCategory(cat) {
-  if (cat === "All") return renderAds();
-  const filtered = ads.filter((ad) => ad.category === cat);
-  renderAds(filtered);
-}
-
 // Start
-renderAds();
 
-async function searchAds() {
-  const term = document.getElementById("searchInput").value;
-  const cat = document.querySelector(".active-category")?.innerText || "All";
+renderAds(ads);
 
-  // This calls the server with specific "query parameters"
-  const response = await fetch(`${API_URL}?search=${term}&category=${cat}`);
-  const filteredAds = await response.json();
-  renderAds(filteredAds);
+// reader.onload = async function (event) {
+//   const img = new Image();
+//   img.src = event.target.result;
+
+//   img.onload = async function () {
+//     const canvas = document.createElement("canvas");
+//     const MAX_WIDTH = 800;
+//     const scaleSize = MAX_WIDTH / img.width;
+//     canvas.width = MAX_WIDTH;
+//     canvas.height = img.height * scaleSize;
+
+//     const ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+//     // Convert to a smaller JPEG instead of a heavy PNG
+//     const compressedBase64 = canvas.toDataURL("image/jpeg", 0.7);
+
+//     const adData = {
+//       title: document.getElementById("adTitle").value,
+//       image: compressedBase64,
+//       // ... other fields
+//     };
+
+//     await fetch(API_URL, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(adData),
+//     });
+//     loadAds();
+//   };
+// };
+
+function cardItem(ad) {
+  const adItem = document.createElement("div");
+  adItem.classList.add("ad-card");
+  adItem.id = ad.id;
+  const adImage = document.createElement("div");
+  adImage.classList.add("ad-image");
+  const adInfo = document.createElement("div");
+  adInfo.classList.add("ad-info");
+  adInfo.innerHTML = `<small>${ad.category}</small>
+  <h3>${ad.title}</h3>
+  <p class="ad-price">$${ad.price}</p>`;
+  const adRmBtn = document.createElement("button");
+  adRmBtn.innerText = "Remove Item";
+  adRmBtn.classList.add("ad-removeBtn");
+  adRmBtn.addEventListener("click", removeAdItem);
+
+  adItem.appendChild(adImage);
+  adItem.appendChild(adInfo);
+  adItem.appendChild(adRmBtn);
+
+  return adItem;
 }
 
-reader.onload = async function (event) {
-  const img = new Image();
-  img.src = event.target.result;
-
-  img.onload = async function () {
-    const canvas = document.createElement("canvas");
-    const MAX_WIDTH = 800;
-    const scaleSize = MAX_WIDTH / img.width;
-    canvas.width = MAX_WIDTH;
-    canvas.height = img.height * scaleSize;
-
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    // Convert to a smaller JPEG instead of a heavy PNG
-    const compressedBase64 = canvas.toDataURL("image/jpeg", 0.7);
-
-    const adData = {
-      title: document.getElementById("adTitle").value,
-      image: compressedBase64,
-      // ... other fields
-    };
-
-    await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(adData),
-    });
-    loadAds();
-  };
-};
+function removeAdItem() {
+  //
+  console.log(event.target.parentElement.id);
+  // event.target.parentElement.remove();
+  
+  const latestAds = ads.filter((ad) => ad.id != event.target.parentElement.id);
+  localStorage.setItem("myAds", JSON.stringify(latestAds));
+  // loadAds()
+  console.log({ latestAds });
+  ads = latestAds;
+  
+  renderAds();
+}
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+.alert('successfully deleted');
